378cb38e99ff2e11981a77293bfd7e05fb7e2815
Caleb Blevins <blevsca2@gmail.com>
2025-03-14 14:57:56 -0400

    Pushing changes to repo.

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ const paper = 1
 
 const scissors = 2
 
-function getComputerChoice (x) {
-    return Math.floor(Math.random() * x);
+const aNumber = (window.prompt("Rock...Paper...Scissors...SHOOT!", "0 = Rock, 1 = Paper 2 = Scissors"))
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice
+
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice () {
+    let x = Math.floor(Math.random(0,2) * 3)
+    if (x == 0) return ("rock")
+    if (x == 1) return ("paper")
+    if (x == 2) return ("scissors")
 }
 console.log(getComputerChoice(3))
 
-prompt (rock, 0)
-prompt (paper, 1)
-prompt (scissors, 2)
-
 function getHumanChoice (y) {
-    return prompt(y)
+    if (y == 0) return ("rock")
+    if (y == 1) return ("paper")
+    if (y == 2) return ("scissors")
 }
-console.log(getHumanChoice(3))
+console.log(getHumanChoice(aNumber))
+
+function playRound (humanChoice, getComputerChoice) {
+}
+
+function playGame(game) {
+
+}
